d84ec3110a44f2e6c707976d599c75a4b690bc9d
c0de-witcher <adityamsrivastava9@gmail.com>
2025-11-01 15:08:34 +0530

    Initial commit

---
+++ src/main/java/com/example/FarmYukti/FarmYuktiApplication.java
@@ -0,0 +1,13 @@
+package com.example.FarmYukti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmYuktiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmYuktiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FarmYukti/FarmYuktiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FarmYukti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmYuktiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
